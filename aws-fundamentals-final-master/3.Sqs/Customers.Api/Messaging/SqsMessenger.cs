c6e67ef74d678a8f4996a0bd2cabc5172adf610d
Gerardo Chechic <Gerardo.Chechic@endava.com>
2023-06-28 20:53:07 -0300

    init

---
+++ aws-fundamentals-final-master/3.Sqs/Customers.Api/Messaging/SqsMessenger.cs
@@ -0,0 +1,49 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace Customers.Api.Messaging
+{
+    public class SqsMessenger : ISqsMessenger
+    {
+        private readonly IAmazonSQS _sqs;
+        private readonly IOptions<QueueSettings> _queueSettings;
+        private string? _queueUrl;
+        public SqsMessenger(IAmazonSQS sqs, IOptions<QueueSettings> queueSettings)
+        {
+            _sqs = sqs;
+            _queueSettings = queueSettings;
+        }
+
+        public async Task<SendMessageResponse> SendMessageAsync<T>(T message)
+        {
+            var sendMessageRequest = new SendMessageRequest
+            {
+                QueueUrl = await GetQueueUrlAsync(),
+                MessageBody = JsonSerializer.Serialize(message),
+                MessageAttributes = new Dictionary<string, MessageAttributeValue> {
+                    {
+                        "MessageType", new MessageAttributeValue
+                        {
+                            DataType="String",
+                            StringValue=typeof(T).Name
+                        }
+                    } }
+            };
+
+            return await _sqs.SendMessageAsync(sendMessageRequest);
+        }
+
+        private async Task<string> GetQueueUrlAsync()
+        {
+            if (_queueUrl is not null)
+            {
+                return _queueUrl;
+            }
+            GetQueueUrlResponse queueUrlResponse = await _sqs.GetQueueUrlAsync(_queueSettings.Value.Name);
+            _queueUrl = queueUrlResponse.QueueUrl;
+            return _queueUrl;
+        }
+    }
+}
---
+++ aws-fundamentals-final-master/Customers.Consumer/Program.cs
@@ -0,0 +1,16 @@
+using Amazon.SQS;
+using Customers.Consumer;
+using Customers.Consumer.Messaging;
+using MediatR;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<QueueSettings>(builder.Configuration.GetSection(QueueSettings.Key));
+builder.Services.AddSingleton<IAmazonSQS, AmazonSQSClient>();
+builder.Services.AddHostedService<QueueConsumerService>();
+builder.Services.AddMediatR(typeof(Program));
+
+var app = builder.Build();
+
+
+app.Run();
